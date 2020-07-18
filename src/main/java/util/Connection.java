3a583e6e3036ecdb4bca7843f5a0dd065ab65815
Vladislav Kutsobin <vladislavkutsobin@gmail.com>
2020-07-18 14:58:59 +0200

    Added Connection class

---
+++ src/main/java/util/Connection.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
+class ConnectionToDB {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/testDB?serverTimezone="+
+            TimeZone.getDefault().getID(); //fix SQLException --> The server time zone value 'EEST' is unrecognized
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "spring";
+
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+    public static Connection getConnectionToDB() throws SQLException, ClassNotFoundException {
+        Class.forName(JDBC_DRIVER);
+        return DriverManager.getConnection(DATABASE_URL,USERNAME,PASSWORD);
+    }
+}
